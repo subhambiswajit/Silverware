cc5ad5b6aa0e57a0462d8b3093afcbac26b45a5e
subhambiswajit <digu35@gmail.com>
2022-11-27 18:57:23 -0500

    deploy backend app

--- frontend/silverware/src/environments/environment.dev.ts
+++ frontend/silverware/src/environments/environment.dev.ts
@@ -1,7 +1,6 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-/* istanbul ignore next */
 export const environment = {
   production: false,
   firebaseConfig: {
@@ -13,7 +12,7 @@ export const environment = {
     appId: "1:840383312064:web:bf50c1c0d04de3f9a83d25",
     measurementId: "G-7KT90GFH61"
   },
-  apiServer: 'http://localhost:8000',
+  apiServer: 'https://backend-yopxzgdubq-uc.a.run.app',
   apiUrls: {
     registerUser: '/users/register/',
     loginUser: '/signin/',
